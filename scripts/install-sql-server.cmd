258b1b8dda1206b827217e967fd4c07e703283c2
Kai Armstrong <kai.armstrong@gmail.com>
2015-09-24 12:58:18 -0500

    update for different sql version

--- scripts/install-sql-server.cmd
+++ scripts/install-sql-server.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-echo Installing SQL Server 2014 Express, it will take a while...
-C:\vagrant\resources\SQLEXPR_x64_ENU.exe /Q /Action=install /INDICATEPROGRESS /INSTANCENAME="SQLEXPRESS" /INSTANCEID="SQLExpress" /IAcceptSQLServerLicenseTerms /FEATURES=SQL,Tools /TCPENABLED=1 /SECURITYMODE="SQL" /SAPWD="#SAPassword!"
+echo Installing SQL Server 2008 Express, it will take a while...
+C:\vagrant\resources\SQLEXPRWT_x64_ENU.exe /Q /Action=install /INDICATEPROGRESS /INSTANCENAME="SQLEXPRESS" /INSTANCEID="SQLExpress" /IAcceptSQLServerLicenseTerms /FEATURES=SQL,Tools /TCPENABLED=1 /SECURITYMODE="SQL" /SAPWD="#SAPassword!"
 
 echo Disabling firewall
 netsh advfirewall set allprofiles state off
